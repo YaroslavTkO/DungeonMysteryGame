45f82231606dffe848abf2f9e45bbf6963f292b6
YaroslavMs <Yaroscro@gmail.com>
2022-02-17 13:55:50 +0200

    downloaded GUI asset

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -8,11 +8,11 @@ public class PlayerStats : MonoBehaviour
     {
         Idle,
         Walking,
-        Rolling,
         Attacking
     }
 
     public float hp = 100;
+    public float stamina = 100;
     public float movementSpeed = 5;
     public float damageMultiplier = 1.0f;
     public float experience = 0;
